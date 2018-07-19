1ee76e74c065c4ba51780a588af303dc4136ee1d
kmssankar <s.sankarganesh@hotmail.com>
2018-07-19 08:40:24 +0530

    added enumeration

---
+++ CoreJavaProj01/src/org/corejava/enumerators/EnumeratorClass.java
@@ -0,0 +1,49 @@
+package org.corejava.enumerators;
+
+//Enumerator
+enum AccCntrl
+{
+	defaultAcc,publicAcc,protectedAcc,PrivateAcc
+}
+
+enum Construct
+{
+  User1("Sankar"),User2("Udhaya"),User3("Rajesh");	
+    private String name;
+    public String Getname()
+    {
+    	return this.name;
+    }
+	Construct(String Name)
+	{
+		this.name=Name;
+	}
+}
+public class EnumeratorClass {
+	
+	public static void main(String[] Args)
+	{
+		// Creating instance of enum <No need to use new Keyword !>
+		AccCntrl Acs ;
+		// Assign a value to Acs.
+		Acs = AccCntrl.publicAcc;
+		System.out.println("PublicAcc "+ Acs);
+		AccCntrl Acs2 ;
+		Acs2 = AccCntrl.defaultAcc;
+		AccCntrl Acs3 ;
+		Acs3 = AccCntrl.publicAcc;
+		
+		String Message = (Acs == Acs2 )? "Enum equals Acs1 and Ac2" : " Acs1 and Ac2 Not Equal";
+		{
+			System.out.println(Message);
+		}
+		
+		if (Acs == Acs3 )
+		{
+			System.out.println("Enum equals Acs and Ac3");
+		}
+		//get Enum from constructor
+		System.out.println("Enum from constructor :"+Construct.User1.Getname());
+		
+	}
+}
